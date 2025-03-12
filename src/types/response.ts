26d7b6bc7f2f8566ab0121173292ccd9358e0447
Cesar Martinez <cesarwamartinez@gmail.com>
2025-03-12 17:21:07 -0500

    feat(types) Se añadio algunos types sencillos en types repo

---
+++ src/types/response.ts
@@ -0,0 +1,28 @@
+
+interface Categoria {
+	descripcion: string
+	id_categoria: number
+	nombre: string
+}
+
+
+
+
+
+enum CodigoStatus {
+	200,
+	401,
+	404,
+	500,
+	409
+};
+
+
+
+
+// Status enum aqui
+
+interface Response {
+	message: string
+	status: 
+}
