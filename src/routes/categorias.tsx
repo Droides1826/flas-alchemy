21eef2ad0966527514fa973ce193d846077e58e0
Cesar Martinez <cesarwamartinez@gmail.com>
2025-03-13 14:09:18 -0500

    Add new routers and render

--- src/routes/categorias.tsx
+++ src/routes/categorias.tsx
@@ -1,8 +1,20 @@
 import { useEffect, useState } from "react";
 import { Response } from "../types/response";
+import { Data } from "../types/response";
 
 export default function Categorias() {
   const [data, setData] = useState<Response>();
+
+  const handleDeleteCategoria = (object: Data) => {
+    fetch("http://127.0.0.1:5000/categorias", {
+      method: "POST",
+      body: JSON.stringify({
+        nombre: object.nombre,
+        descripcion: object.descripcion,
+        id_categoria: object.id_categoria,
+      }),
+    });
+  };
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/categorias")
@@ -14,7 +26,7 @@ export default function Categorias() {
     return (
       <>
         <div className="flex gap-2 flex-col">
-          {data.data.map((item) => (
+          {data.data.map((item, index) => (
             <div className="flex flex-row rounded-md p-3 border border-zinc-200 justify-between">
               <div>
                 <p className="font-semibold text-xl">{item.nombre}</p>
@@ -26,7 +38,37 @@ export default function Categorias() {
                   <button className="btn btn-neutral">Eliminar</button>
                 </div>
                 <div>
-                  <button className="btn btn-primary">Actualizar</button>
+                  <button
+                    className="btn"
+                    onClick={() =>
+                      document.getElementById("my_modal_3").showModal()
+                    }
+                  >
+                    Actualizar
+                  </button>
+                  <dialog id="my_modal_3" className="modal">
+                    <div className="modal-box">
+                      <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                          ✕{" "}
+                        </button>
+                      </form>
+                      <h3 className="font-bold text-lg">Hello {index}!</h3>
+                      <p className="py-4">
+                        {item.descripcion}
+                      </p>
+                      <div>
+                        <form className="flex flex-col" onSubmit={(e) => {
+                            e.preventDefault()
+                        }}>
+                            <input type="text" placeholder="Descripcion" />
+                            <input type="text" placeholder="Nombre" />
+                            <input type="text" placeholder="" />
+                        </form>
+                      </div>
+                    </div>
+                  </dialog>
                 </div>
               </div>
             </div>
